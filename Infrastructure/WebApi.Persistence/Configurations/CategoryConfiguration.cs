005769670ee52e9fc55db8bfa7d0871834caab7c
aliemreyesilyurt <aliemree.y@hotmail.com>
2024-11-26 11:55:07 +0300

    Configurations was created for Entities by using IEntityTypeConfiguration and Bogus package

---
+++ Infrastructure/WebApi.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            Category category1 = new Category()
+            {
+                Id = 1,
+                Name = "Elektronik",
+                Priorty = 1,
+                ParentId = 0,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now,
+            };
+
+            Category category2 = new Category()
+            {
+                Id = 2,
+                Name = "Moda",
+                Priorty = 2,
+                ParentId = 0,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now,
+            };
+
+            Category parent1 = new Category()
+            {
+                Id = 3,
+                Name = "Bilgisayar",
+                Priorty = 1,
+                ParentId = 1,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now,
+            };
+
+            Category parent2 = new Category()
+            {
+                Id = 4,
+                Name = "Kadin",
+                Priorty = 1,
+                ParentId = 2,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now,
+            };
+
+            builder.HasData(category1, category2, parent1, parent2);
+        }
+    }
+}
---
+++ Infrastructure/WebApi.Persistence/Configurations/DetailConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Persistence.Configurations
+{
+    public class DetailConfiguration : IEntityTypeConfiguration<Detail>
+    {
+        public void Configure(EntityTypeBuilder<Detail> builder)
+        {
+            Faker faker = new Faker();
+
+            Detail d1 = new Detail()
+            {
+                Id = 1,
+                Title = faker.Lorem.Sentence(1),
+                Description = faker.Lorem.Sentence(5),
+                CategoryId = 1,
+                CreatedDate = DateTime.Now,
+                IsDeleted = false,
+            };
+
+            Detail d2 = new Detail()
+            {
+                Id = 2,
+                Title = faker.Lorem.Sentence(1),
+                Description = faker.Lorem.Sentence(5),
+                CategoryId = 3,
+                CreatedDate = DateTime.Now,
+                IsDeleted = true,
+            };
+
+            Detail d3 = new Detail()
+            {
+                Id = 3,
+                Title = faker.Lorem.Sentence(1),
+                Description = faker.Lorem.Sentence(5),
+                CategoryId = 4,
+                CreatedDate = DateTime.Now,
+                IsDeleted = false,
+            };
+
+            builder.HasData(d1, d2, d3);
+        }
+    }
+}
