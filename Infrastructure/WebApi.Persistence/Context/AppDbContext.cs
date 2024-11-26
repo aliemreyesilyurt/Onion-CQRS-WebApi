ae8c66c6648c28773c3814ebb22a7929f919b69b
aliemreyesilyurt <aliemree.y@hotmail.com>
2024-11-26 12:51:40 +0300

    Created and used Registration method on Program.cs Migration created and updated the database

---
+++ Infrastructure/WebApi.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Detail> Details { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
