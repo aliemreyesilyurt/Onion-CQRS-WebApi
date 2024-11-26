e92abd36fe29cb450c16aa095b7336106ecb4013
aliemreyesilyurt <aliemree.y@hotmail.com>
2024-11-26 15:30:59 +0300

    Repositories structures was created and used (Write Repository)

--- Core/WebApi.Application/Interfaces/Repositories/IWriteRepository.cs
+++ Core/WebApi.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -5,5 +5,9 @@ namespace WebApi.Application.Interfaces.Repositories
     public interface IWriteRepository<T> : IRepository<T>
         where T : class, IBaseEntity, new()
     {
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IList<T> entities);
+        Task<T> UpdateAsync(T entity);
+        Task HardDeleteAsync(T entity);
     }
 }
--- Infrastructure/WebApi.Persistence/Registration.cs
+++ Infrastructure/WebApi.Persistence/Registration.cs
@@ -15,6 +15,7 @@ namespace WebApi.Persistence
                 opt.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString")));
 
             services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
+            services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
         }
     }
 }
