82a57bb9bf3527024ba5631a7184b54e87d98b02
aliemreyesilyurt <aliemree.y@hotmail.com>
2024-11-27 12:06:22 +0300

    AutoMapper was integrated into the project

---
+++ Core/WebApi.Application/DTOs/BrandDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApi.Application.DTOs
+{
+    public class BrandDto
+    {
+        public string Name { get; set; }
+    }
+}
--- Core/WebApi.Domain/Entities/Product.cs
+++ Core/WebApi.Domain/Entities/Product.cs
@@ -4,19 +4,6 @@ namespace WebApi.Domain.Entities
 {
     public class Product : BaseEntity
     {
-        public Product()
-        {
-
-        }
-
-        public Product(string title, string description, decimal price, decimal discount, int brandId)
-        {
-            Title = title;
-            Description = description;
-            Price = price;
-            Discount = discount;
-            BrandId = brandId;
-        }
 
         public string Title { get; set; }
         public string Description { get; set; }
